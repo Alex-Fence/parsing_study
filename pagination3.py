a421ad59525b664c4dc9a78c456d481576851761
Alex-Fence <bokuto@mail.ru>
2024-10-26 18:28:33 +0300

    создал функции make_soup, make_list_of_goods_items_links, load_cost_counts_by_good, load_cost_counts_by_page

--- pagination3.py
+++ pagination3.py
@@ -6,11 +6,43 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-url = 'https://parsinger.ru/html/index1_page_1.html'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
+
+def make_soup(url):
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+def make_list_of_goods_items_links(url):
+    soup = make_soup(url)
+    # формирование ссылок на товары страницы
+    item_cards = soup.select('a.name_item')
+    item_links_lst = [item.get('href') for item in item_cards]
+    print(item_links_lst)
+    return item_links_lst
+
+def load_cost_counts_by_good(url):
+    soup = make_soup(url)
+    in_stock = int(soup.find('span', id='in_stock').text[11:]) # товара в наличии
+    price = int(soup.find('span', id='price').text[:-4]) # цена товара
+    cost = in_stock * price
+    print(f'{url}: {in_stock}, {price}, {cost}')
+    return cost
+
+def load_cost_counts_by_page(page_url):
+    goods_lst = make_list_of_goods_items_links(page_url)
+    sum_costs = sum([load_cost_counts_by_good(main_url+url_good) for url_good in goods_lst])
+    return sum_costs
+
+
+
+
+main_url = 'https://parsinger.ru/html/'
+url1 = 'https://parsinger.ru/html/index1_page_1.html'
+soup = make_soup(url1)
+# список ссылок из раздела пагинации
 links_lst = [link['href'] for link in soup.find('div', class_='pagen').find_all('a')]
 print(links_lst)
-item_cards = soup.select('a.name_item')
-item_links_lst = [item.get('href') for item in item_cards]
-print(item_links_lst)
+
+print(load_cost_counts_by_good(main_url+make_list_of_goods_items_links(url1)[0]))
+print(f'{links_lst[0]}: {load_cost_counts_by_page(main_url+links_lst[0])}')
