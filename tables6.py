e5175ac6e562a4ff54559b2cf574b447ac33cfce
Alex-Fence <bokuto@mail.ru>
2024-11-10 22:36:39 +0300

    6я задача с парсингом таблиц со страниц готово тут еще бонус в конце результат пишется в буфер обмена

--- tables6.py
+++ tables6.py
@@ -18,14 +18,21 @@ if __name__ == '__main__':
     url ='https://parsinger.ru/table/5/index.html'
     soup = make_soup(url)
     rows = soup.find('table').find_all('tr')
-    res_columns_dict = {td.text: 0 for td in rows[0]}
-    print(res_columns_dict)
+    # получение названий столбцов
+    headers_lst = [th.text for th in rows[0].find_all('th')]
+    # создание списка для записи суммированых значений по каждому столбцу
+    result_lst = [0 for _ in range(len(headers_lst))]
     for row in rows[1:]:
-        for key in res_columns_dict.keys():
-            for td in row.find_all('td'):
-                res_columns_dict[key] += float(td.text)
-    res_columns_dict.pop('\n')
+        # получаем список значений по данной строке
+        row_data = [float(td.text) for td in row.find_all('td')]
+        # добавляем значения в результирующий список
+        for i in range(len(headers_lst)):
+            result_lst[i] += row_data[i]
+    # объединяем список заголовков с результирующим списком в словарь
+    res_columns_dict = dict(zip(headers_lst, result_lst))
+    # округление значений по условию задачи
     res_columns_dict = {k: round(v, 3) for k, v in res_columns_dict.items()}
     print(res_columns_dict)
+    # запись результата в буфер обмена для вставки в веб-сайт
     pyperclip.copy(str(res_columns_dict))
 
