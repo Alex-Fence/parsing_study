bf7c8067a42e5730a550a04a8e4735167cd8ec50
Alex-Fence <bokuto@mail.ru>
2024-11-16 16:59:24 +0300

    8я задача с парсингом таблиц со страниц готова

--- tables8.py
+++ tables8.py
@@ -17,11 +17,11 @@ def make_tables(tables):
 if __name__ == '__main__':
     url_link = 'https://parsinger.ru/4.8/8/index.html'
     soup = make_soup(url_link)
-    tables_all = soup.find_all('table')
-
-    # for table in tables:
-    #     print(table.find_all('td', attrs={'colspan': True}))
-
-    print(tables[1].find_all('table'))
-    #colspan_data = [table.find_all('th', attrs={'colspan': True}) for table in tables]
-    #print(colspan_data)
+    # числа из заголовков таблиц
+    th_lst = soup.find_all('th', attrs={'colspan': True})
+    th_sum = sum([int(th.text) for th in th_lst])
+    # числа из ячеек
+    td_row = soup.select('td[colspan]')
+    td_lst = [int(td.text) for td in td_row[0].find_all('td', attrs={'colspan': True})]
+    td_sum = sum(td_lst)
+    print(td_sum + th_sum)
