d1f8452ea3364ad1085818fb3877d4262f6d7e6a
Alex-Fence <bokuto@mail.ru>
2024-11-18 15:50:44 +0300

    Задание 9 исправил сортировку, ГОТОВО

--- tables 9.py
+++ tables 9.py
@@ -29,26 +29,28 @@ if __name__ == "__main__":
     soup: BeautifulSoup = make_soup(url_link)
     # номера пунктов которые нужно выбрать
     num_items_lst: list = [0, 1, 4, 7]
-
+    # список ключей для словаря
     headers_lst: list = [th.text for th in soup.find_all("th")]
     headers_lst = [th for th in headers_lst if headers_lst.index(th) in num_items_lst]
-    # print(headers_lst)
+
     rows_lst: list = [tr for tr in soup.find_all("tr")]
+    # словарь для списков, который будет передаваться в json
     json_lst: list = []
     for row in rows_lst[1:]:
-         # print(row.find_all("td"))
-         row_dt_lst : list = [dt.text for dt in row]
-         row_dt_lst = [dt for dt in row_dt_lst if row_dt_lst.index(dt) in num_items_lst]
-         row_dt_lst = [int(row_dt) if row_dt_lst.index(row_dt) in (1, 3) else row_dt for row_dt in row_dt_lst]
-         # Cтоимость не выше 4 000 000 (Стоимость авто <= 4000000),
-         # Год выпуска начиная с 2005 года (Год выпуска >= 2005),
-         # Тип двигателя - Бензиновый (Тип двигателя == "Бензиновый").
-         row_dict = dict(zip(headers_lst, row_dt_lst))
-         if row_dict['Стоимость авто'] <= 4000000 and row_dict['Год выпуска'] >= 2005 and row_dict['Тип двигателя'] == "Бензиновый":
+        # получил список значений без тегов
+        row_dt_lst : list = [dt.text for dt in row]
+        # убрал лишние значения в списке
+        row_dt_lst = [dt for dt in row_dt_lst if row_dt_lst.index(dt) in num_items_lst]
+        # преобразовал год выпуска и цену с числовой формат
+        row_dt_lst = [int(row_dt) if row_dt_lst.index(row_dt) in (1, 3) else row_dt for row_dt in row_dt_lst]
+        # получил словарь значений
+        row_dict = dict(zip(headers_lst, row_dt_lst))
+        # проверка на соответствие фильтру, если да то добавляю в словарь
+        if row_dict['Стоимость авто'] <= 4000000 and row_dict['Год выпуска'] >= 2005 and row_dict['Тип двигателя'] == "Бензиновый":
             json_lst.append(row_dict)
-    sorted(json_lst, key=lambda x: x["Стоимость авто"])
+    # сортировка полученного списка словарей
+    json_lst = sorted(json_lst, key=lambda x: x["Стоимость авто"])
+    # валидация для json
     sorted_cars = json.dumps(json_lst, indent=4, ensure_ascii=False)
-    for item in json_lst:
-        print(item)
     print(sorted_cars)
     pyperclip.copy(sorted_cars)
