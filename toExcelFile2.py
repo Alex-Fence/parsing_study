7ca7ff6f2b8a6644f1d62240b6ea5df9c633a168
Alex-Fence <bokuto@mail.ru>
2024-11-28 23:06:40 +0300

    Done!!!

--- toExcelFile2.py
+++ toExcelFile2.py
@@ -2,22 +2,25 @@
 # Вам потребуется заходить в каждую товарную карточку и собирать данные, отмеченные на
 # предоставленном изображении.
 # Сохраните данные в формате CSV с разделителем ;
+import csv
+import os
 import requests
 from bs4 import BeautifulSoup
 
-def make_soup(url_link: str)->BeautifulSoup:
+# создание супа
+def make_soup(url_link: str) -> BeautifulSoup:
     response = requests.get(url_link)
     response.encoding = 'utf-8'
     return BeautifulSoup(response.text, 'lxml')
+
 
 # формируем основную часть строки данных по продукту
 def get_good_data(good_link: str) -> list[str]:
     description_lst = []
     cur_soup: BeautifulSoup = make_soup(good_link)
-    good_name = cur_soup.find('p', id = 'p_header').text
-    print(good_name)
+    good_name = cur_soup.find('p', id='p_header').text
     description_lst.append(good_name)
-    article = cur_soup.find('p', class_ = 'article').text.split(': ')[1]
+    article = cur_soup.find('p', class_='article').text.split(': ')[1].strip()
     description_lst.append(article)
     li_items = [i.text.split(': ')[1] for i in cur_soup.find_all('li')]
     for li_item in li_items:
@@ -26,25 +29,44 @@ def get_good_data(good_link: str) -> list[str]:
     description_lst.append(cur_soup.find('span', id='price').text)
     description_lst.append(cur_soup.find('span', id='old_price').text)
     description_lst.append(good_link)
-    print(description_lst)
     return description_lst
 
 
 # формирование записи строк в общий список со страницы
-def add_to_main_rows_list(page_link: str)-> None:
-    pass
+def add_to_main_rows_list(page_link: str) -> None:
+    soup_page: BeautifulSoup = make_soup(page_link)
+    goods_links_by_page_lst: list[str] = [item.get('href') for item in soup_page.select('a.name_item')]
+    print(goods_links_by_page_lst)
+    for goods_link in goods_links_by_page_lst:
+        rows_lst.append(get_good_data(main_url + goods_link))
+
+
+# запись данных в файл
+def write_to_csv(filename: str) -> None:
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    with open(filename, 'a', encoding='utf-8-sig') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(t_headers_lst)
+        for row in rows_lst:
+            writer.writerow(row)
+
 
 if __name__ == '__main__':
-    t_headers_lst: list[str] = ['Наименование', 'Артикул', 'Бренд', 'Модель', 'Тип', 'Технология экрана', 'Материал корпуса', 'Материал браслета', 
-    'Размер', 'Сайт производителя', 'Наличие', 'Цена', 'Старая цена', 'Ссылка на карточку с товаром']
+    t_headers_lst: list[str] = ['Наименование', 'Артикул', 'Бренд', 'Модель', 'Тип', 'Технология экрана',
+                                'Материал корпуса', 'Материал браслета',
+                                'Размер', 'Сайт производителя', 'Наличие', 'Цена', 'Старая цена',
+                                'Ссылка на карточку с товаром']
     firs_url = 'https://parsinger.ru/html/index1_page_1.html'
     main_url = 'https://parsinger.ru/html/'
     soup: BeautifulSoup = make_soup(firs_url)
-    # сбор наименований продуктов на текущей странице
-    # watchs_names_lst: list[str] = [i.text for i in soup.find_all('a', class_= 'name_item')]
-    # print(watchs_names_lst)
 
-    rows_lst = [] # сюда записывать строки для последующей таблицы
-    goods_links_by_page_lst: list[str] = [item.get('href') for item in soup.select('a.name_item')]
-    print(goods_links_by_page_lst)
-    get_good_data(main_url+goods_links_by_page_lst[0])
+    rows_lst = []  # сюда записывать строки для последующей таблицы
+    pagen_lst = soup.find('div', class_='pagen').find_all('a', href=True)
+    # получить список страниц из пагинации
+    pagen_lst: list[str] = [i.get('href') for i in pagen_lst]
+    for page_link in pagen_lst:
+        add_to_main_rows_list(main_url + page_link)
+    # запись данных в файл
+    write_to_csv('data.csv')
