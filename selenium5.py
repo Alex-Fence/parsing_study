e98f1e89833e336173114948a9b89a54b2315b85
Alex-Fence <bokuto@mail.ru>
2024-12-23 16:38:09 +0300

    5я задача по selenium выполнена

---
+++ selenium5.py
@@ -0,0 +1,23 @@
+# Взлом Кодового Замка: Откройте веб-сайт с помощью Selenium.
+# Активация Чек-боксов: Найдите все чек-боксы на странице и установите их в положение checked с помощью .click().
+# Открывание Секрета: Как только все чек-боксы будут активированы, нажмите на кнопку.
+# Доступ к Секретным Данным: Скопируйте число, которое появится в теге <p id="result">Result</p>.
+
+import pyperclip
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+url = 'http://parsinger.ru/selenium/4/4.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    checkboxes = browser.find_elements(By.CLASS_NAME, 'check')
+    for checkbox in checkboxes:
+        checkbox.click()
+    browser.find_element(By.CLASS_NAME, 'btn').click()
+    res = browser.find_element(By.ID, 'result').text
+    print(res)
+    time.sleep(10)
+pyperclip.copy(str(res))
+print(res)
+
