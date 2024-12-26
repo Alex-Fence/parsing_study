f8bc40b0a8675a45ababc8178c7ff8d932ed6d3f
Alex-Fence <bokuto@mail.ru>
2024-12-26 17:10:03 +0300

    Задание 7 по Selenium ГОТОВО

--- selenium7.py
+++ selenium7.py
@@ -3,6 +3,21 @@
 # Дешифровка Кода: Сложите (плюсуйте) все извлеченные значения.
 # Использование Кода: Вставьте получившийся результат в поле на сайте и нажмите кнопку.
 # Получение Конечного Результата: Копируйте длинное число, которое появится после нажатия на кнопку.
+import time
 
 import pyperclip
-import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+url = 'https://parsinger.ru/selenium/7/7.html'
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    options_lst = [int(num.text) for num in browser.find_elements(By.TAG_NAME, 'option')]
+    sum_code = sum(options_lst)
+    browser.find_element(By.ID, 'input_result').send_keys(str(sum_code))
+    time.sleep(5)
+    browser.find_element(By.CLASS_NAME, 'btn').click()
+    res = browser.find_element(By.ID, 'result').text
+print(res)
+pyperclip.copy(str(res))
