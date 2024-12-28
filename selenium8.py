c05b29c103086c04c880232eb67308c49df7df5c
Alex-Fence <bokuto@mail.ru>
2024-12-28 13:38:52 +0300

    8я задача по selenium ГОТОВО

--- selenium8.py
+++ selenium8.py
@@ -10,14 +10,17 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-from selenium5 import checkboxes
-
 url1 = 'https://parsinger.ru/selenium/6/6.html'
 temp_key = ((12434107696 * 3) * 2) + 1
 
 with webdriver.Chrome() as browser:
     browser.get(url1)
-    checkboxes = [num.click() for num in browser.find_elements(By.TAG_NAME, 'option') if int(num.text) == temp_key]
+    # Ключ к Выпадающему Списку: Откройте выпадающий список и выберите элемент с числом, которое у вас получилось на предыдущем этапе.
+    [num.click() for num in browser.find_elements(By.TAG_NAME, 'option') if int(num.text) == temp_key]
+    # нажатие кнопки
     browser.find_element(By.CLASS_NAME, 'btn').click()
-    time.sleep(10)
+    res = browser.find_element(By.ID, 'result').text
+    # копирование результирующего значения в буфер обмена
+    pyperclip.copy(str(res))
+    time.sleep(5)
 
